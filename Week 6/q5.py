b0271bd92341cf56f7a02edcca7a9531df0b683f
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2025-12-30 20:21:44 +0545

    Week 6 Lab Report Complete

---
+++ Week 6/q5.py
@@ -0,0 +1,11 @@
+def string_to_int(a, b):
+    a = int(a)   # explicit conversion from string to integer
+    return a + b
+
+
+# Example
+a = "123"
+b = 102
+
+result = string_to_int(a, b)
+print(result)   # Output: 225
---
+++ Week 6/q6.py
@@ -0,0 +1,4 @@
+def my_func(a ,b ,c=3):
+    print("Sum is: ", a+b+c)
+
+my_func(2,4)
