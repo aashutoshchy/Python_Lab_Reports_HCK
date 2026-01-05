2e2bbc68e8b95f492b064824fc9d8cb213e6c61c
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-01-05 22:34:05 +0545

    Q2 complete

--- Week 7/two.py
+++ Week 7/two.py
@@ -1,1 +1,11 @@
 # Write a program to count the number of upper-case alphabets present in a text file “PYTHON.TXT”.
+
+count = 0
+
+with open("PYTHON.TXT", "r") as file:
+    data = file.read()
+    for ch in data:
+        if ch.isupper():
+            count += 1
+
+print("The numbe of Uppercase is: ", count)
